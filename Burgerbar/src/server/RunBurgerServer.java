28fd6ca5c324478a8f73de9e346759531e34ec5b
273962 <48473715+273962@users.noreply.github.com>
2019-11-06 14:39:21 +0100

    je

---
+++ Burgerbar/src/server/RunBurgerServer.java
@@ -0,0 +1,24 @@
+package server;
+
+import server.model.burger.BurgerQueue;
+import server.model.burger.BurgerQueueHandler;
+import server.network.ServerRMI;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class RunBurgerServer {
+    public static void main(String[] args) {
+        BurgerQueue burgerQueue = new BurgerQueueHandler();
+
+        try {
+            Registry reg = LocateRegistry.createRegistry(1099);
+            ServerRMI server = new ServerRMI(burgerQueue);
+            reg.bind("burgerServer", server);
+        } catch (RemoteException | AlreadyBoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Burgerbar/src/server/network/ServerRMI.java
@@ -0,0 +1,28 @@
+package server.network;
+
+import Shared.BurgerBar;
+import server.model.burger.BurgerQueue;
+import server.model.domain.Burger;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ServerRMI implements BurgerBar {
+    BurgerQueue burgerQueue;
+
+    public ServerRMI(BurgerQueue burgerQueue) throws RemoteException {
+        UnicastRemoteObject.exportObject(this, 0);
+        this.burgerQueue = burgerQueue;
+    }
+
+    @Override
+    public void produceBurger(Burger burger) {
+
+    }
+
+    @Override
+    public Burger consumeBurger() {
+        return null;
+    }
+}
